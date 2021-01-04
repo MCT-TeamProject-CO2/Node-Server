e2d9830d69b4a3e94834963d7b14aed9e79f8a8e
Yimura <andreas.maerten@scarlet.be>
2021-01-04 18:13:26 +0100

    fix(Index): Added catch all/last resort

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import Main from './src/Main.js'
 
 const main = new Main();
 main.start();
+
+process.on('uncaughtException', (err) => main.log.critical('PROCESS', 'An uncaught error occured:', err));
