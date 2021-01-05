92c552728260481539584caee6612417e4277ced
Yimura <andreas.maerten@scarlet.be>
2021-01-05 17:21:30 +0100

    fix(Index): Set logging level in index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import Main from './src/Main.js'
+import log from './src/util/Log.js'
+
+log.setLogLevel('VERBOSE'); // Valid log levels: VERBOSE/INFO/WARNING/ERROR/CRITICAL
 
 const main = new Main();
 main.start();
