a99b372dc8971c7cbbc07102276c481f98b74953
Yimura <andreas.maerten@scarlet.be>
2021-01-05 17:21:06 +0100

    fix(Route): Added log and modules getter

--- src/structures/routes/Route.js
+++ src/structures/routes/Route.js
@@ -3,7 +3,15 @@ export default class Route {
      * @param {Main} main The program entrypoint class
      */
     constructor(main) {
+        this._m = main;
+    }
 
+    get log() {
+        return this._m.log;
+    }
+
+    get modules() {
+        return this._m.modules;
     }
     
     get route() {
