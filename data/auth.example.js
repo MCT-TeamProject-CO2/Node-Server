b9a40d3663426699acad5da554e4ecab1fade065
Yimura <andreas.maerten@scarlet.be>
2021-01-06 17:45:22 +0100

    refactor(Data): Made auth  example up to date

--- data/auth.example.js
+++ data/auth.example.js
@@ -1,7 +1,30 @@
 export default {
+    ms_oauth: {
+        tenant: 'common', // see => https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-v2-protocols#endpoints
+        clientId: '',
+        clientSecret: '',
+        scopes: [ 'openid' ]
+    },
+
     influx_db: {
-        url: 'http://host.example.com:8086',
+        url: '',
         token: '',
-        organisation: ''
+        organisation: '',
+        bucket: ''
+    },
+    mongo_db: {
+        auth: {
+            host: "",
+            user: "",
+            password: "",
+            database: "",
+            port: 27017
+        },
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        }
     }
 };
