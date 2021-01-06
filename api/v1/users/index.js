ebde365d7e54d0969e065faad00c2599bdae82ae
Yimura <andreas.maerten@scarlet.be>
2021-01-06 17:45:39 +0100

    feat(API): Added users endpoint

---
+++ api/v1/users/index.js
@@ -0,0 +1,86 @@
+import Route from '~/src/structures/routes/Route.js'
+
+/**
+ * Route: /api/v1/users
+ */
+export default class Users extends Route {
+    constructor(main) {
+        super(main);
+    }
+
+    get route() {
+        return '';
+    }
+
+    get model() {
+        return this.modules.user.model;
+    }
+
+    /**
+     * Disable a specific user
+     * @param {Rquest} request 
+     */
+    async delete(request) {
+        const body = await request.json();
+        if (!body) return request.reject(400);
+
+        return request.accept(
+            await this.model.disableUser(body),
+            200
+        );
+    }
+
+    /**
+     * Return all the registered users
+     * @param {Request} request 
+     */
+    async get(request) {
+        return request.accept(
+            await this.model.getUsers({})
+        );
+    }
+
+    /**
+     * Create a new user
+     * @param {Request} request 
+     */
+    async post(request) {
+        const body = await request.json();
+        if (!body) return request.reject(400);
+
+        try {
+            await this.model.createUser(body);
+
+            return request.accept('', 201);
+        } catch (error) {
+            console.log(error);
+
+            return request.reject(406, {
+                code: 406,
+                status: "406 - Not Acceptable",
+                message: error.message
+            });
+        }
+    }
+
+    /**
+     * Update a user
+     * @param {Request} request 
+     */
+    async put(request) {
+        const body = await request.json();
+        if (!body || !body.query || !body.update) return request.reject(400);
+        
+        try {        
+            return request.accept(
+                await this.model.updateUser(body.query, body.update)
+            );
+        } catch (error) {
+            return request.reject(406, {
+                code: 406,
+                status: "406 - Not Acceptable",
+                message: error.message
+            });
+        }
+    }
+}
