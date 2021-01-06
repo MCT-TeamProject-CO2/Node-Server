2f9917f2a3d70aefb52786334719d2a909d500ac
Yimura <andreas.maerten@scarlet.be>
2021-01-06 17:44:49 +0100

    feat(Modules): Added POC Teams module

---
+++ src/modules/Teams/index.js
@@ -0,0 +1,53 @@
+import BaseModule from './structures/BaseModule.js'
+import fetch from 'node-fetch'
+import { WebHookUrl } from './util/Constants.js'
+
+export default class Teams extends BaseModule {
+    constructor(main) {
+        super(main);
+
+        this.register(Teams, {
+            disabled: true,
+            name: 'teams'
+        });
+    }
+
+    /**
+     * send a teams message to howest staff
+     * @param {string} title 
+     * @param {string} message 
+     */
+    async postMessageToTeams(title, message) {
+        const card = {
+            '@type': 'MessageCard',
+            '@context': 'http://schema.org/extensions',
+            themeColor: "0072C6", // light blue
+            summary: 'Test Summary',
+            sections: [
+                {
+                    activityTitle: title,
+                    text: message,
+                },
+            ],
+        };
+
+        const res = await fetch(WebHookUrl, {
+            headers: {
+                'Content-Type': 'application/vnd.microsoft.teams.card.o365connector',
+                'Content-Length': `${card.toString().length}`
+            },
+            method: 'POST',
+            body: JSON.stringify({ card })
+        });
+        
+        return res.ok;
+    }
+}
+
+
+// import pymsteams
+// channel="https://studenthowest.webhook.office.com/webhookb2/276f5711-8096-4d28-8b4c-3390fd10aa42@4ded4bb1-6bff-42b3-aed7-6a36a503bf7a/IncomingWebhook/bb12bd4350c248f68351decb7fc1be4b/4e4d40df-20c9-4deb-88d1-e99a697d25a6"
+// msg="This text is sent in Python - by TDC"
+// myTeamsMessage = pymsteams.connectorcard(channel)
+// myTeamsMessage.text(msg)
+// myTeamsMessage.send()
---
+++ src/modules/Teams/util/Constants.js
@@ -0,0 +1,5 @@
+export const WebHookUrl = "https://studenthowest.webhook.office.com/webhookb2/276f5711-8096-4d28-8b4c-3390fd10aa42@4ded4bb1-6bff-42b3-aed7-6a36a503bf7a/IncomingWebhook/bb12bd4350c248f68351decb7fc1be4b/4e4d40df-20c9-4deb-88d1-e99a697d25a6";
+
+export default {
+    WebHookUrl
+};
