ab86c0d542ee53092a3e7d4a9dbafb05fe32e9c0
Yimura <andreas.maerten@scarlet.be>
2021-01-07 21:23:18 +0100

    feat(API): Added location endpoint

---
+++ api/v1/locations.js
@@ -0,0 +1,49 @@
+import Route from '~/src/structures/routes/Route.js'
+
+export default class Location extends Route {
+    constructor(main) {
+        super(main);
+    }
+
+    get model() {
+        return this.modules.location.model;
+    }
+
+    get route() {
+        return '/locations';
+    }
+
+    /**
+     * 
+     * @param {Request} request 
+     */
+    async get(request) {
+        if (!await this.isSessionValid(request)) return request.reject(403);
+
+        return request.accept(
+            await this.model.getAll()
+        );
+    }
+
+    /**
+     * 
+     * @param {Request} request 
+     */
+    async post(request) {
+        if (!await this.isSessionValid(request)) return request.reject(403);
+
+        const body = await request.json();
+        if (!body) return request.reject(400);
+
+        try {
+            await this.model.createIfNotExists(body);
+        } catch (error) {
+            return request.reject(406, {
+                code: 406,
+                status: "406 - Not Acceptable",
+                message: error.message
+            });
+        }
+        return request.accept('', 201);
+    }
+}
