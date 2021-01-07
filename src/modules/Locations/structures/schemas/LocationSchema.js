0c5c659a97cd87a379f536ca52c103ad2af5fd4b
Yimura <andreas.maerten@scarlet.be>
2021-01-07 21:22:41 +0100

    fix(LocationSchema): Added x,y position to rooms

--- src/modules/Locations/structures/schemas/LocationSchema.js
+++ src/modules/Locations/structures/schemas/LocationSchema.js
@@ -4,6 +4,10 @@ import mongoose from 'mongoose'
 const room = new mongoose.Schema({
     name: { type: String, required: true },
     tag: { type: String, required: true },
+    position: {
+        x: Number,
+        y: Number
+    }
 });
 
 const floor = new mongoose.Schema({
@@ -14,13 +18,13 @@ const floor = new mongoose.Schema({
 
 const building = new mongoose.Schema({
     name: { type: String, required: true },
-    tag: { type: String, required: true }, 
+    tag: { type: String, required: true },
     floors: [floor]
 });
 
 const location = new mongoose.Schema({
     name: { type: String, required: true },
-    tag: { type: String, required: true },
+    tag: { type: String, required: true, unique: true },
     buildings: [building]
 });
 
