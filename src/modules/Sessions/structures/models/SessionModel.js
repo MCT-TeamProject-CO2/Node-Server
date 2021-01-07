f110be3aa45ec9badd69cdf0d983bb9bd9411e03
Yimura <andreas.maerten@scarlet.be>
2021-01-07 14:36:44 +0100

    docs(SessionModel): Fixed documentation comments

--- src/modules/Sessions/structures/models/SessionModel.js
+++ src/modules/Sessions/structures/models/SessionModel.js
@@ -4,6 +4,7 @@ import SessionSchema from '../schemas/SessionSchema.js'
  * Insert a session document for a certain user id
  * @param {string} uid The user's UUID
  * @param {string} sessionId A session id
+ * @returns {Promise<Object>}
  */
 export const createSession = (uid, sessionId) => {
     return new SessionSchema({
@@ -15,7 +16,7 @@ export const createSession = (uid, sessionId) => {
 /**
  * Just checks if a session exists, if you need the session use getSession
  * @param {string} sessionId
- * @returns {boolean}
+ * @returns {Promise<boolean>}
  */
 export const doesSessionExist = async (sessionId) => {
     const doc = await getSession(sessionId);
@@ -26,6 +27,7 @@ export const doesSessionExist = async (sessionId) => {
 /**
  * Returns the entire session document
  * @param {string} sessionId 
+ * @returns {Promise<Object>}
  */
 export const getSession = (sessionId) => {
     return SessionSchema.findOne({ sessionId }).exec();
@@ -34,6 +36,7 @@ export const getSession = (sessionId) => {
 /**
  * Removes the session from the DB
  * @param {string} sessionId 
+ * @returns {Promise<Object>}
  */
 export const revokeSession = (sessionId) => {
     return SessionSchema.deleteOne({ sessionId }).exec();
