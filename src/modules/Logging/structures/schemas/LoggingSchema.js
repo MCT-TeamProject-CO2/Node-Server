dcb3f34d7eba65d9ba951a3a540e2e79e9759473
Yimura <andreas.maerten@scarlet.be>
2021-01-18 19:24:26 +0100

    feat(LoggingSchema): Added current date (default)

--- src/modules/Logging/structures/schemas/LoggingSchema.js
+++ src/modules/Logging/structures/schemas/LoggingSchema.js
@@ -4,7 +4,8 @@ const logs = new mongoose.Schema({
     level: { type: String, required: true },
     type: { type: String, required: true },
     message: { type: String, required: true },
-    technical: { type: String, default: null }
+    technical: { type: String, default: null },
+    datetime: { type: Date, required: true, default: Date }
 });
 
 export default mongoose.model('logs', logs);
