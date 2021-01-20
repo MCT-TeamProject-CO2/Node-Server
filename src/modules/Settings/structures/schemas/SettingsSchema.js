92db232ac248f2bae1356f8cfd7f48b556bc4b54
Yimura <andreas.maerten@scarlet.be>
2021-01-20 18:04:28 +0100

    fix(SettingsSchema): Thresholds should be numbers

--- src/modules/Settings/structures/schemas/SettingsSchema.js
+++ src/modules/Settings/structures/schemas/SettingsSchema.js
@@ -4,8 +4,8 @@ const settings = new mongoose.Schema({
     id: { type: Number, required: true, default: 1 },
     config: {
         ppmThresholds: {
-            orange: { type: String, required: true, default: 900 },
-            red: { type: String, required: true, default: 1500 }
+            orange: { type: Number, required: true, default: 900 },
+            red: { type: Number, required: true, default: 1500 }
         },
         disableNormalLogin: { type: Boolean, required: true, default: false }
     }
