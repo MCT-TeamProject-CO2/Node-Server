bfa2c75546d32e60ab0691d67b09f5a9ccc2b7d3
Yimura <andreas.maerten@scarlet.be>
2021-01-25 14:25:49 +0100

    feat(LocationSchema): Added floor plan schema

--- src/modules/Locations/structures/schemas/LocationSchema.js
+++ src/modules/Locations/structures/schemas/LocationSchema.js
@@ -1,4 +1,3 @@
-import { stringToLines } from '@influxdata/influxdb-client';
 import mongoose from 'mongoose'
 
 const room = new mongoose.Schema({
@@ -22,10 +21,16 @@ const building = new mongoose.Schema({
     floors: [floor]
 });
 
+const floor_plan = new mongoose.Schema({
+    tag: { type: Number, required: true },
+    svg: String
+});
+
 const location = new mongoose.Schema({
     name: { type: String, required: true },
     tag: { type: String, required: true, unique: true },
-    buildings: [building]
+    buildings: [building],
+    floor_plans: [floor_plan]
 });
 
 export default mongoose.model('locations', location);
