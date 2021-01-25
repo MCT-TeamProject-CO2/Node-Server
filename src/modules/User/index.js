fa029ffb52a16f866760faf4f651d32f50d451ac
Yimura <andreas.maerten@scarlet.be>
2021-01-25 14:22:55 +0100

    feat(Modules/user): Create initial root user
    Co-authored-by: vanGoethem Joren <joren.vangoethem@student.howest.be>

--- src/modules/User/index.js
+++ src/modules/User/index.js
@@ -1,6 +1,7 @@
 import BaseModule from './structures/BaseModule.js'
 import UserModel from './structures/models/UserModel.js'
 import Constants from './util/Constants.js'
+import crypto from 'crypto'
 
 export default class User extends BaseModule {
     /**
@@ -14,6 +15,23 @@ export default class User extends BaseModule {
             name: 'user',
             requires: [ 'mongo' ]
         });
+    }
+
+    async init() {
+        const admin = await this.model.getUser({ username: "admin" });
+        if (!admin) {
+            const randomPass = crypto.randomBytes(32).toString('base64');
+            this.log.info("User", `Password for the admin account: ${randomPass}`);
+            const newAdmin = {
+                "email": "admin@example.com",
+                "type": "normal",
+                "permission": "root",
+                "username": "admin",
+                "password": randomPass
+            };
+            this.model.createUser(newAdmin);
+        }
+        return true;
     }
 
     get constants() {
