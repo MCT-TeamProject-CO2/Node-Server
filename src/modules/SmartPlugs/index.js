c19a0731dd45ed9e14061862c087ad94f433469e
Yimura <andreas.maerten@scarlet.be>
2021-01-25 14:25:13 +0100

    refactor(Modules): Only existing smartplugs toggle
    To prevent API spam we've added a check to see if a SmartPlug exists.
    Co-authored-by: vanGoethem Joren <joren.vangoethem@student.howest.be>

--- src/modules/SmartPlugs/index.js
+++ src/modules/SmartPlugs/index.js
@@ -5,6 +5,7 @@ const login = tplinkApi.login;
 // const tplink = await login("jorenvangoethem@hotmail.com", "JorenvanGoethem51", uuidv4());
 
 export default class SmartPlugs extends BaseModule {
+    _cache = new Map();
     tplink;
 
     constructor(main) {
@@ -21,7 +22,7 @@ export default class SmartPlugs extends BaseModule {
             const config = this.auth.tp_link;
             this.tplink = await login(config.email, config.password, uuidv4());
                         
-            await this.tplink.getDeviceList();
+            await this.getDevices();
 
             this.log.info('TP Link', 'Established connection to TP Link account successfully.');
         } catch (error) {
@@ -35,18 +36,22 @@ export default class SmartPlugs extends BaseModule {
         return SettingsModel;
     }
 
-    async getDevices(){
+    async getDevices() {
         try{
-            await this.tplink.getDeviceList();
-        }catch (error){
+            const deviceList = await this.tplink.getDeviceList();
+            this._cache.clear();
+            deviceList.forEach(device => this._cache.set(device.alias, true));
+        }catch (error) {
             return false;
         }
-       return true;
+        return true;
     }
 
     async toggle(tagString, toggle = true) {
         try {
-            await this.tplink.getHS100(tagString)[toggle ? 'powerOn' : 'powerOff']();
+            if (this._cache.has(tagString)) {
+                await this.tplink.getHS100(tagString)[toggle ? 'powerOn' : 'powerOff']();
+            }
         } catch (error) {   
             this.log.error("SmartPlugs", "Toggle error:", error);
         }
