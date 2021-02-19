cbcc9e1c246fa596afdd378fa071a0ae773e3eed
Yimura <andreas.maerten@scarlet.be>
2021-02-19 15:47:49 +0100

    wip(Cluster): Added multithreaded basics

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,5 @@
+import cluster from 'cluster'
+import os from 'os'
 import ModuleManager from './managers/Modules.js'
 import log from './util/Log.js'
 import auth from '~/data/auth.js'
@@ -21,7 +23,30 @@ export default class Main {
         return this._modulesManager;
     }
 
+    /**
+     * 
+     * @param {*} worker 
+     * @param {*} code 
+     * @param {*} signal 
+     */
+    onSlaveExit(worker, code , signal) {
+        this.log.critical('CLUSTER', `Worker "${worker.process.pid}" died`);
+    }
+
     start() {
+        if (cluster.isMaster) {
+            this.log.info('CLUSTER', 'Master has started.');
+
+            const forks = this.config.development ? 1 : os.cpus().length;
+            for (let i = 0; i < forks; i++)
+                cluster.fork();
+
+            cluster.on('exit', this.onSlaveExit.bind(this));
+            return;
+        }
+
+        this.log.info('CLUSTER', `Slave has started PID ${process.pid}`);
+
         this._modulesManager.load();
     }
 }
